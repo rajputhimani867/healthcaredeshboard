a17441a438f039fbe7c777eee9576bb197645493
HIMANI RAJPUT <rajputhimani867@gmail.com>
2025-05-28 19:43:35 +0530

    changes

--- src/components/HealthStatusCards.js
+++ src/components/HealthStatusCards.js
@@ -8,7 +8,7 @@ function HealthStatusCards({ data }) {
         <div key={index} className="status-card">
           <div className="status-header">
             <span className="status-icon" style={{ backgroundColor: card.color }}></span>
-            <span className="status-organ">{card.organ}</span>
+            {/* <span className="status-organ">{card.organ}</span> */}
             <span className="status-date">{card.date}</span>
           </div>
           <span className="status-details">Details</span>
